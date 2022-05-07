5195c041444d7a42f0809a1c46d5707c90d9f064
清水ハイネ <heine.shimizu@gmail.com>
2022-05-07 20:38:08 +0900

    #3 api記載方法修正

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,11 @@
 import  { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) => {
+export default function handler({
+  req,
+  res
+}: {
+  req: NextApiRequest
+  res: NextApiResponse
+}) => {
   res.status(200).json({ text: 'Hello' })
 }
